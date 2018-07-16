117675afb09ee57beadd360f343c9ec9d68dbe78
cboswell <cboswell@middlebury.edu>
2018-07-16 14:27:54 -0400

    debugging issue with storage of tags and user tags

--- insta_dispatch_tests.c
+++ insta_dispatch_tests.c
@@ -35,10 +35,7 @@ main(int argc, char* argv[])
 
 	dis.user_id = 1234;
 	dis.timestamp = time(NULL);
-	dis.audience_size = 3;
-	for (int i = 0; i < dis.audience_size; i++){
-		dis.audience[i] = (i + 1)*1000;
-	}
+	dis.audience_size = 0;
 
 	dis.num_tags = 3;	
 
@@ -48,34 +45,95 @@ main(int argc, char* argv[])
 	
 	dis.num_user_tags = 4;
 
-	dis.user_tags[0] = 1111;	
-	dis.user_tags[1] = 2222;
-	dis.user_tags[2] = 3333;
-	dis.user_tags[3] = 4444;
+	dis.user_tags[0] = 1;	
+	dis.user_tags[1] = 2;
+	dis.user_tags[2] = 3;
+	dis.user_tags[3] = 4;
 	
 	parent.type = 0;
 	parent.id = 1234;
 	dis.parent = &parent;
 
 	dis.fragmentation = 5;
-	dis.dispatch_id = 9949;
+	dis.dispatch_id = 6666;
 
 	insert_dispatch(&dis);
 
-	dis.dispatch_id = 8734;
+	/* 1 - dispach with shared parent id */
+
+	body.media_path = "comment:";
+	body.text = "Cool cat";
+	dis.body = &body;
+
+	dis.user_id = 3;
+	dis.timestamp = time(NULL);
+	dis.audience_size = 0;
+
+	dis.num_tags = 0;	
+
+	dis.num_user_tags = 2;
+
+	dis.user_tags[0] = 1;	
+	dis.user_tags[1] = 4;
+	
+	parent.type = 1;
+	parent.id = 6666;
+	dis.parent = &parent;
+
+	dis.fragmentation = 5;
+	dis.dispatch_id = 1;
+
+	insert_dispatch(&dis);
+
+	/* 2 - dispatch with shared parent id */
+
+	body.media_path = "comment:";
+	body.text = "Nice smile!";
+	dis.body = &body;
+
+	dis.user_id = 2;
+	dis.timestamp = time(NULL);
+	dis.audience_size = 0;
+
+	dis.num_tags = 0;	
+	
+	dis.num_user_tags = 0;
+	
+	parent.type = 1;
+	parent.id = 6666;
+	dis.parent = &parent;
+
+	dis.fragmentation = 5;
+	dis.dispatch_id = 2;
+
 	insert_dispatch(&dis);
 	
-	/*
-	 * Testing delete_dispatch method in insta_dispatch_definions.c 
-	 */
+	/* 3 - dispatch with shared parent id */
 
-	uint64_t dispatch_id = 9949; 
-	
-//	delete_dispatch(dispatch_id);
+	body.media_path = "comment:";
+	body.text = "wow, great picture";
+	dis.body = &body;
 
-	printf("\n\n\nprinting dipatch with id %ld\n", dispatch_id);
+	dis.user_id = 4;
+	dis.timestamp = time(NULL);
+	dis.audience_size = 0;
+
+	dis.num_tags = 0;	
 	
-	search_dispatch_by_id(dispatch_id);	
+	dis.num_user_tags = 0;
+	
+	parent.type = 1;
+	parent.id = 6666;
+	dis.parent = &parent;
+
+	dis.fragmentation = 5;
+	dis.dispatch_id = 3;
+
+	insert_dispatch(&dis);
+	
+
+	/* search by parent's dispatch id */
+	search_dispatch_by_parent_id((uint64_t) 6666, 3);	
 
 }
 
